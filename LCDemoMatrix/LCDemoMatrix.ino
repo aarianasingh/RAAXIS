f4ed23076c77bf444b928b5429973e042191e429
xy4song <xy4song@uwaterloo.ca>
2019-11-02 14:32:20 -0400

    prototype done

--- LCDemoMatrix/LCDemoMatrix.ino
+++ LCDemoMatrix/LCDemoMatrix.ino
@@ -1,8 +1,7 @@
-
 //We always have to include the library
 #include "LedControl.h"
 const int rightButton = 4;
-
+const int leftButton = 3;
 /*
   Now we need a LedControl to work with.
  ***** These pin numbers will probably not work with your hardware *****
@@ -35,14 +34,14 @@ void setup() {
   }
 
   pinMode (rightButton, INPUT);
-
+  pinMode (leftButton, INPUT);
   for (int i = 0; i < 16; ++i) {
     shape[i] = 0;
     background[i] = 0;
   }
   //0 is top of the screen, set shape at top
-  shape[0] = B01000000;
-  shape[1] = B11100000;
+  shape[0] = B00010000;
+  shape[1] = B00111000;
 
   //The shapes can all be draw in a 5x2 grid. The first 4 bits represent the top column, last 4 bits represent the bottom column.
   s[0] = B01001110;
@@ -51,7 +50,7 @@ void setup() {
   s[3] = B11100010;
   s[4] = B11110000;
   s[5] = B11000110;
-  //randomSeed(analogRead(0));//insert pin number that is unconnected!;
+  randomSeed(analogRead(0));//insert pin number that is unconnected!;
 }
 
 byte generateShape () {
@@ -86,7 +85,7 @@ void moveDown() {
     //Serial.print(shape[i]);
   }
   shape[0] = 0;
-  delay(delaytime);
+  //delay(delaytime);
 }
 void moveLeft() {
   byte check_shape_shift = 1;
@@ -126,216 +125,109 @@ void moveRight() {
 }//end of method
 
 void wipeShapeMatrix() {
-  for (int i = 15; i > 0; --i) {
+  for (int i = 15; i >= 0; --i) {
     shape[i] = 0;
   }
 }//end of method
 
-void Place_Shape()
-{
+void wipeGameMatrix() {
+  for (int i = 15; i >= 0; --i) {
+    background[i] = 0;
+  }
+}
 
-  byte check_place_shape = 1;
+bool bottomDetection() {
   for (int i = 15; i >= 0; i--)
   {
     if (((shape[i]& background[i + 1]) != B00000000)) //checks if shift causes it to reach a shape or the end row
     {
-      check_place_shape = 0;
-
-      break;
+      return 0;
     }
   }
+  return 1;
+}
+void placeShape() {
+  for (int i = 15; i >= 0; --i) {
+    background[i] = background[i] | shape[i];
+  }
+}
+void clearRows() {
+  int i = 15;
+  while (background[i] == B11111111) {
+    --i;
+  }
+  int diff = 15 - i;
+  for (int j = i; j >= 0; --j) {
+    background[j + diff] = background[j];
+  }
+  for (int j = 0; j < diff; ++j) {
+    background[j] = 0;
+  }
+
+  /*for (int i = 15; i >= 0; --i) {
+    if (background[i] == B11111111)
+    {
+      for (int k = i; k > 0; k--) //moves the rows above cleard line down
+      {
+        background[k] = background[k - 1];
+      }
+      background[0] = 0;
+      player_score++;
+    }
+    }*/
+}
+//clears the shape matrix and places the newly generated shape at the begining of the shape matrix
+//should call random generate shape function and change the shape
+void Place_Shape()
+{
+
+  bool check_place_shape = bottomDetection();
   if (check_place_shape == 0)
   {
-    for (int i = 15; i >= 0; --i) {
+    placeShape();
+    clearRows();
+    wipeShapeMatrix();
 
-      background[i] = background[i] | shape[i];
-      if (background[i] == B11111111)
-      {
-        for (int k = i; k > 0; k--) //moves the rows above cleard line down
-        {
-          background[k] = background[k - 1];
-        }
-        background[0] = 0;
-        player_score++;
-      }
-
-      if (background[0] != B00000000) game_state = 0;
-
+    if (background[0] == B00000000 || background[1] == B00000000)
+    {
+      byte rShape = generateShape();
+      shape[0] = topCol(rShape);
+      shape[1] = bottomCol(rShape);
     }
-
-    //clears the shape matrix and places the newly generated shape at the begining of the shape matrix
-    //should call random generate shape function and change the shape
-
+    else {
+      game_state = 0;
+      wipeGameMatrix();
+    }
   }
 }//end of method
-
 void loop() {
-  //int addr = 0;
-  // int counter = 1;
+
   bool placed = true;
+  lc.setRow(0, 0, B00000000);
 
-  int rightButtonState = digitalRead(rightButton);
+  int counter = 0;
 
-  if (rightButtonState == LOW) {
-    lc.setRow(0, 0, B10000000);
+  while (game_state) {
+      updateGraphics();
+    int rightButtonState = digitalRead(rightButton);
+    int leftButtonState = digitalRead(leftButton);
+
+    if (rightButtonState == LOW) {
+      //lc.setRow(0, 0, B10000000);
+      moveRight();
+    }
+    if (leftButtonState == LOW) {
+      //lc.setRow(0, 0, B00000001);
+      moveLeft();
+    }
+    if (counter % 5 == 0) {
+      moveDown();
+    }
+    Place_Shape();
+
+    counter ++;
   }
-  else {
-    lc.setRow(0, 0, B00000000);
-  }
-
-  /*if (counter%16==0)
-    {
-    byte rShape = generateShape();
-    wipeShapeMatrix();
-    shape[0] = topCol(rShape);
-    shape[1] = bottomCol(rShape);
-    delay (1000);
-    }*/
+  wipeGameMatrix();
+  wipeShapeMatrix();
   updateGraphics();
-  moveDown();
-
-  moveLeft();
-  Place_Shape();
-  moveRight();
-  Place_Shape();
-
-  moveRight();
-  //delay(delaytime);
-  //++counter;
 }
-
-/*
-  This function lights up a some Leds in a row.
-  The pattern will be repeated on every row.
-  The pattern will blink along with the row-number.
-  row number 4 (index==3) will blink 4 times etc.
-*/
-/*void rows() {
-  for (int row = 0; row < 8; row++) {
-    delay(delaytime);
-    lc.setRow(0, row, B10100000);
-    delay(delaytime);
-    lc.setRow(0, row, (byte)0);
-    for (int i = 0; i < row; i++) {
-      delay(delaytime);
-      lc.setRow(0, row, B10100000);
-      delay(delaytime);
-      lc.setRow(0, row, (byte)0);
-    }
-  }
-  }*/
-
-/*
-  This function lights up a some Leds in a column.
-  The pattern will be repeated on every column.
-  The pattern will blink along with the column-number.
-  column number 4 (index==3) will blink 4 times etc.
-*/
-/*void columns() {
-  for (int col = 0; col < 8; col++) {
-    delay(delaytime);
-    lc.setColumn(0, col, B10100000);
-    delay(delaytime);
-    lc.setColumn(0, col, (byte)0);
-    for (int i = 0; i < col; i++) {
-      delay(delaytime);
-      lc.setColumn(0, col, B10100000);
-      delay(delaytime);
-      lc.setColumn(0, col, (byte)0);
-    }
-  }
-  }*/
-
-/*
-  This function will light up every Led on the matrix.
-  The led will blink along with the row-number.
-  row number 4 (index==3) will blink 4 times etc.
-*/
-//void single() {
-/*for(int row=0;row<8;row++) {
-  for(int col=0;col<8;col++) {
-  delay(delaytime);
-  lc.setLed(1,row,col,true);
-  delay(delaytime);
-  for(int i=0;i<col;i++) {
-    lc.setLed(1,row,col,false);
-    delay(delaytime);
-    lc.setLed(1,row,col,true);
-    delay(delaytime);
-  }
-  }
-  }*/
-
-/*int r1 = 0, c1 = 7, r2 = 0, c2 = 6, r3 = 1, c3 = 6, r4 = 2, c4 = 6;
-  for (int i = 0; i < 4; ++i) {
-  while (c1 >= 0)
-  { lc.setLed(i, r1, c1, true);
-    lc.setLed(i, r2, c2, true);
-    delay(delaytime);
-    --c1;
-    --c2;
-    lc.setLed(i, r1, c1 + 1, false);
-  }
-  if (c1 < 0) {
-    lc.setLed(i, r1, c1 + 1, false);
-    c1 = 7;
-    continue;
-  }
-  }
-  }*/
-
-/*void move_shape(byte game_map[], byte shape[], byte display[])
-  {
-  byte long_tetris = B00111100;
-  byte square_tetris[2];
-  square_tetris[0] = B00001100;
-  square_tetris[1] = B00001100;
-
-  //moving to the right
-  for (int i = 7; i > 1; i--)
-  {
-    if (!(((shape[i - 1] >> 1) && B00000001) == 00000000)) //checks if shift causes it to reach a wall
-    {
-      check_shape_shift = 0;
-      break;
-    }
-  }
-  if (check_shape_shift == 1)
-    for (int i = 8; i > 1; i--)
-    {
-      shape[i] = shape[i - 1] >> 1;
-    }
-  shape[0] == B00000000;
-
-  //moving to the left
-  for (int i = 7; i > 1; i--)
-  {
-    if (!(shape[i - 1] << 1) && B10000001 == 00000000) //checks if shift causes it to reach a wall
-    {
-      check_shape_shift = 0;
-      break;
-    }
-  }
-  if (check_shape_shift == 1)
-    for (int i = 8; i > 1; i--)
-    {
-      shape[i] = shape[i - 1] << 1;
-    }
-  shape[0] == B00000000;
-
-
-  for (int i = 0; i < 8; i++)
-  {
-    if (game_map[i]&shape[i] != B00000000)
-      check_game_move = false;
-
-  }
-  if (check_game_move == true)
-  {
-    for (int i = 0, i < 8; i++)
-      display[i] = shape[i] & game_map[i];
-  }
-  //to check if the shape should be placed onto the game map i.e become static
-  for (int
-
-  }*/
